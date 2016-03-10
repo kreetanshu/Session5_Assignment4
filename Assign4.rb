f380098184f23a5a473d54a9f13e14b795f8a183
reetanshu <kreetanshu@gmail.com>
2016-03-10 18:09:07 +0530

    Assign4

---
+++ Assign4.rb
@@ -0,0 +1,14 @@
+1) Blocks can be denoted with
+ans b) { }
+
+2) ‘a’..’f’ will print
+ans a) a b c d e f
+
+3)1…5 will print
+a) 1 2 3 4
+
+4) Procedures are denoted with ………… class
+a) Proc
+
+5)Proc and Lambda differs in
+a) Argument
